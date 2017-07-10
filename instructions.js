5169eff0045456cd081e95ca8d033b77daf23b70
Dylan Brown <dylandoesprogramming@gmail.com>
2017-07-10 16:38:00 -0500

    Got a simple modal implemented

---
+++ instructions.js
@@ -0,0 +1,19 @@
+console.log("Instructions are loaded!");
+
+//Get the modal
+var modal = document.getElementById('myModal');
+
+//Get the <span> that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//open on load
+window.onload = function(){
+  console.log("Function on trigger!");
+  modal.style.display = "block";
+};
+
+window.onclick = function(event) {
+  if(event.target == modal || event.target == span) {
+    modal.style.display = "none";
+  }
+};
